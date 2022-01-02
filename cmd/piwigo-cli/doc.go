d2a7378de1f692c2a3ebee874365e232c73dd043
celogeek <65178+celogeek@users.noreply.github.com>
2022-01-02 18:13:49 +0100

    start writing doc

---
+++ cmd/piwigo-cli/doc.go
@@ -0,0 +1,25 @@
+// Copyright 2011 Celogeek. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// Piwigo Cli
+//
+// This tools allow you to interact with your Piwigo Instance.
+// Installation:
+//  go get github.com/celogeek/piwigo-cli/cmd/piwigo-cli
+//
+// Help
+//
+// To get help:
+//  piwigo-cli -h
+//
+// QuickStart
+//
+// Login
+//
+// First connect to your instance:
+//  piwigo-cli session login -u URL -l USER -p PASSWORD
+//
+// Then check your status
+//  piwigo-cli session status
+package main
