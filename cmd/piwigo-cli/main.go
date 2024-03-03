b6a8f508685de36bc319f4a67abf111968495174
celogeek <65178+celogeek@users.noreply.github.com>
2024-03-03 10:52:18 +0100

    fix help error

--- cmd/piwigo-cli/main.go
+++ cmd/piwigo-cli/main.go
@@ -15,14 +15,9 @@ var parser = flags.NewParser(&options, flags.Default)
 
 func main() {
 	if _, err := parser.Parse(); err != nil {
-		switch flagsErr := err.(type) {
-		case flags.ErrorType:
-			if flagsErr == flags.ErrHelp {
-				os.Exit(0)
-			}
-			os.Exit(1)
-		default:
-			os.Exit(1)
+		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
 		}
+		os.Exit(1)
 	}
 }
